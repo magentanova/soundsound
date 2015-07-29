ff9c4285504ff7ca2f5d00773e2f21eb31e1ffde
Justin Richards <justinrichards@Justins-MacBook-Air.local>
2015-07-29 12:51:09 -0500

    added playlist view and search functionality

--- js/app-browserify.js
+++ js/app-browserify.js
@@ -10,17 +10,18 @@ var Backbone = require('Backbone')
 
 var React = require('react')
 
+var _ = require('Underscore')
+
 var clientId = '5b0d197fa9c0a7e2f9ccdf975be97e8a',
 	secret = '85f60ef57548dd8eff54f32023da205e',
 	endUserAuthorization = 'https://soundcloud.com/connect',
 	token = 'https://api.soundcloud.com/oauth2/token'
 
-console.log('loaded');
+window._ = _
 
 //IIFE
 
-
-
+console.log('loaded2')
 
 var Track = Backbone.Model.extend({
 
@@ -31,61 +32,141 @@ var Track = Backbone.Model.extend({
 
 	initialize: function(trackId) {
 		this.trackId = trackId
-		console.log(trackId)
+		
 		this.url = `https://api.soundcloud.com/tracks/${this.trackId}?client_id=${clientId}`
-	},
-
-	parse: function(results) {
-		console.log('lockngajfd')
-		console.log(results)
-		return results
 	}
 })
+
+var Playlist = Backbone.Collection.extend({
+
+	defaults: {
+		query: '',
+		error: ''
+	},
+
+	fetchQuery: function(q) {
+		this.query = q
+		this.genUrl()
+		console.log('fetching')
+		this.fetch().then((results)=> {
+			console.log(results)
+			if (results.length === 0) this.error = "no results found"
+			this.trigger('change')
+		})
+	},
+
+	genUrl: function() {
+		this.url = `https://api.soundcloud.com/tracks?client_id=${clientId}&q=${this.query}`
+	},
+
+	initialize: function() {
+		this.genUrl()	
+	}
+})
+
+
+class Bar extends React.Component {
+
+	constructor(props) {
+		super(props)
+		this.state = {
+			ballLeft: '0px',
+			width: '400px'
+		}
+	}
+
+	componentDidMount() {
+		var x = 7 
+	}
+
+	_ballPosToTime(ballLeft,barWidth){
+		var newTime = (ballLeft * this.props.player._duration) / barWidth
+		return newTime
+}
+
+	_followCursor(e) {
+		e.preventDefault()
+		console.log('following cursor')
+		window.fart2 = this
+		var mouseWithinDiv = e.clientX - parseInt(this.state.leftBorder)
+		this.state.ballLeft = mouseWithinDiv - parseInt(this.refs.ball.props.style.width)/2 + 'px'
+		this.setState({ballLeft:this.state.ballLeft})
+	}
+
+	_finishScrub(e) {
+		this.setState({'dragging':false})
+		window.removeEventListener('mousemove',this.state.dragListener)
+		var newTime = this._ballPosToTime(parseInt(this.state.ballLeft),parseInt(this.state.width))
+		this.props.player.seek(newTime)
+
+	}
+
+	_startScrub(e) {
+		e.preventDefault()
+		window.fart = this
+		console.log('start scrub')
+		this.setState({'dragging':true})
+		var domBar = React.findDOMNode(this)
+		this.state.leftBorder = domBar.getBoundingClientRect().left
+		this.state.dragListener = (e) => this._followCursor.bind(this,e)()
+		window.addEventListener('mousemove',this.state.dragListener)
+
+	}
+
+	render() {
+		
+		var ballStyle = {
+			width:'12px',
+			left:this.state.ballLeft},
+
+			dragFn = this.state.dragging ? (e) => this._followCursor.bind(this,e)() : null
+
+		return (
+			<div style={{width: this.state.width}} className="bar">
+				<div ref="ball" draggable='true' style={ballStyle} className="ball" 
+				onMouseDown={(e) => this._startScrub.bind(this,e)()} 
+				onMouseUp={(e) => this._finishScrub.bind(this,e)()}>
+				</div>
+			</div>
+		)
+	}
+}
 
 class TrackView extends React.Component {
 
 	constructor(props) {
 		super(props)
-		console.log(this)
-		this.state = {playing: "notready"}
-		this.props.trackData.on('sync', () => this.forceUpdate())
-		console.log(this.props.trackData)
+		
+		this.state = {
+			playing: "notready",
+			}
+		this.props.track.on('sync', this.forceUpdate)
+		
 	}
 
-	_dragBall(e) {
-		console.log(this)
-		window.view = this
-		trackData.addEventListener('mousemove',followCursor)
-		trackData.addEventListener('mouseup',unDrag.bind(null,sound))
-	}
+	componentDidMount() {
+		SC.stream(`/tracks/${this.props.track.attributes.id}`,
+ 				(sound) => {
+ 					this.setState({player:sound._player})
+ 				})
 
-	_followCursor(){
-		var mouseWithinDiv = e.clientX - barLeft
-		ball.style.left = mouseWithinDiv - ballWidth/2 + 'px' // position within div is div's left border minus the cursor's position
-	}	
+	}
 
  	_playOrPause() {
-
- 		console.log('invoked and playing with state' + this.state.playing)
+ 		
  		if (this.state.playing == "notready"){
  			this.setState({playing:"playing"})
- 			SC.stream(`/tracks/${this.props.trackData.trackId}`,
- 				(sound) => {
- 					this.state.sound = sound
- 					this.state.sound.play()
- 				})
+ 			this.state.player.play()
  			return
  		}
  		
  		else if (this.state.playing == "notplaying"){ 
- 			console.log('playing')
- 			this.state.sound.play()
+ 			this.state.player.play()
  			this.setState({playing:"playing"})
  		}
 
  		else if (this.state.playing == "playing"){
- 			console.log('pausing')
- 			this.state.sound.pause()
+ 			this.state.player.pause()
  			this.setState({playing:"notplaying"})
  		}
 		
@@ -94,19 +175,44 @@ class TrackView extends React.Component {
 	render() {
 
 		var playState = this.state.playing == "playing" ? "pause": "play"
-		console.log(playState)
 		return (
 			<div className="track-view">
 				<i className={playState + " material-icons"} onClick={this._playOrPause.bind(this)}></i>
-				<div ref="track-data" className="track-data">
-					<p className="title">{this.props.trackData.attributes.permalink}</p>
-					<p className="artist">{this.props.trackData.attributes.title}</p>
-					<div ref="bar" className="bar">
-						<div ref="ball" className="ball" onMouseDown={(e) => this._dragBall.bind(this,e)()}></div>
-					</div>
+				<div ref="trackData" className="track-data">
+					<p className="title">{this.props.track.attributes.permalink}</p>
+					<p className="artist">{this.props.track.attributes.title}</p>
+					<Bar player={this.state.player}></Bar>
 				</div>
 	    	</div>
 		)
+	}
+}
+
+class PlaylistView extends React.Component {
+
+	constructor(props) {
+
+		super(props)
+		console.log(this.props.playlist)
+		this.props.playlist.on('sync', () => this.forceUpdate())	
+	}
+
+	_search(e) {
+		window.e = e
+		var q = e.target.value
+		console.log('calling fetchQuery')
+		this.props.playlist.fetchQuery(q)
+	}
+
+	render() {
+		console.log('rendering pv')
+		return (
+			<div className="playlist-view">
+				<input type="text" onKeyPress={(e) => this._search.bind(this,e)()} defaultValue="enter a search term"></input> 
+				<h1>{this.props.playlist.error}</h1>
+				{this.props.playlist.map((track) => {return <TrackView key={track.id} track={track}></TrackView>})}
+			</div>
+			)
 	}
 }
 
@@ -114,9 +220,13 @@ SC.initialize({
     client_id: clientId,
 })
 
-var track = new Track(77777)
-React.render(<TrackView trackData={track}/>,document.querySelector('.container'))
-track.fetch()
+var playlist = new Playlist()
+
+console.log('made playlist')
+console.log(playlist)
+React.render(<PlaylistView playlist={playlist}/>,document.querySelector('.container'))
+console.log('rendered app')
+playlist.fetch()
 // SC.stream(`/tracks/${track.trackId}`,
 //  				(sound) => {
 //  					sound.play()
